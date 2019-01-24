b04c7c2cc546a0a5b73502de1bdd29948748e134
Sahil Butani <sahilbutani1760@gmail.com>
2019-01-24 15:59:20 +0530

    Update KYC.java

--- KYC.java
+++ KYC.java
@@ -1,50 +1,72 @@
-import java.time.*;
-import java.util.*;
+import java.io.*;
+import java.util.Scanner;
+import java.util.Arrays;
 
-public class KYC
+/*
+ *Pings a given host and computes the median of the time taken to ping.
+ * This program uses a Runtime and Process to write terminal ping
+ * command.
+ */
+
+public class Ping
 {
-   public static void main(String ar[])  {
-         ArrayList<LocalDate> ld=new ArrayList<>();
-       Scanner sc=new Scanner(System.in);
-       int n=sc.nextInt();
-       while(n>0) {
-           String s1 = sc.next();
-           String s2 = sc.next();
-           LocalDate dt_1 = LocalDate.parse(s1);
-           LocalDate dt_2 =  LocalDate.parse(s2 );
+  /*
+   * This function takes ping command and count as input which will execute the ping command
+   * depending upon the count. Prints the median of ping time.
+   */
+  public static void pingCommand(String pingCmd,int count)
+  {
+    try
+     {
+        int sizeOfArray = count;
+        String timeString;
+        float[] time = new float[sizeOfArray];
+        Runtime r = Runtime.getRuntime();
+        Process p = r.exec(pingCmd);
+        BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String inputLine = in.readLine();                                   //reads irrelevant first line
 
-           if (dt_1.compareTo(dt_2) > 0) {
-               ld.add(null);ld.add(null);n--;
-              continue;
+      for (int i = 0; (inputLine = in.readLine()) != null && count >0; i++)
+      {
 
-           }
+            timeString = inputLine.substring(inputLine.lastIndexOf('=') + 1, inputLine.lastIndexOf(' '));
+            //extracting the substring containing time//
+           // System.out.println(timeString);
+            time[i] = Float.parseFloat(timeString);
+            count--;
+        }
+        Arrays.sort(time);
 
-           LocalDate range=dt_1.withYear(dt_2.getYear());
+        if(sizeOfArray%2 == 0)
+        {
+            System.out.println("\n" + "The median is " + (time[sizeOfArray/2]+time[(sizeOfArray/2)-1])/2 + " ms");
+        }
+        else
+        {
+            System.out.println("\n" + "The median is " + time[sizeOfArray/2] + " ms");
+        }
+    }
+    catch (IOException e)
+     {
+        e.printStackTrace();
+    }
+  }
 
-           LocalDate range1=range.minusDays(30);
-           LocalDate range2=range.plusDays(30);
+  /*
+   * Main method takes ip address to ping from a user and number of time user wants
+   * to ping the ip address.Both values are passed to the pingCommand function.
+   */
+    public static void main(String[] args) {
 
-            ld.add(range1);
+        System.out.println("Enter the host ip");
+        Scanner scan = new Scanner(System.in);
+        String ip = scan.next();
+        System.out.println("Enter the number of times the server to be pinged");
+        int count = scan.nextInt();
 
-            if(range2.compareTo(dt_2)>0)
-                ld.add(dt_2);
-          else
-                 ld.add(range2);
-          n--;
-       }
-      for(int i=0;i<ld.size();i++){
-         if(i%2!=0){
-            if(ld.get(i)==null){
-               System.out.println(" RANGE!");
-            continue;}
-         System.out.println(ld.get(i));
-           }
-         else{
-            if(ld.get(i)==null){
-               System.out.print("NO");
-            continue;}
-            System.out.print(ld.get(i)+" ");
-         }
-      }
-   }
+        String pingCmd = "ping " + ip + " -c" + count;                          //string of ping command
+
+          pingCommand(pingCmd,count);
+
+    }
 }
